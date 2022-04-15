587bd121d5a4bb8cd4295fa1904f721d26082445
Mackery <70108834+homelikezero@users.noreply.github.com>
2022-04-15 15:36:04 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Guess a Number copilot
  Guess a number between 1 and 10 with 1 guess
+
+## Programs used
+- Github Copilot
+- Visual Studio Code
---
+++ number guess.py
@@ -0,0 +1,52 @@
+# Pick a number between 1 and 10 and try to guess it.
+
+# pick a random number between 1 and 10
+import random
+
+print('I am thinking of a number between 1 and 10.');
+number = random.randint(1, 10);
+guess = '';
+guessList = [];
+guessCount = 0;
+print('you have 1 guess.');
+
+while guess != number:
+    # get a guess from the user
+    guess = int(input('What is your guess? '));
+    # add the guess to the list
+    guessList.append(guess);
+    # count the number of guesses
+    guessCount += 1;
+    # check if the guess is correct
+    if guess == number:
+        print('Good job! You guessed my number!');
+    else:
+        print('Nope. Better luck next time!');
+
+    # check if the guess is valid
+    if not guess < 1 or not guess > 10:
+        print('sorry, you must guess a number between 1 and 10');
+
+    # check if the user has guessed 1 time
+    if guessCount == 1:
+        print('You have guessed 1 time.');
+        print('you have failed');
+        print('you were not able to guess the number');
+        print('the number was', number);
+        print('you guessed', guessList);
+        break
+    
+# check if the player wants to play again
+playAgain = input('Do you want to play again? (y/n) ');
+if playAgain == 'y':
+    guess = '';
+    guessList = [];
+    guessCount = 0;
+    number = random.randint(1, 10);
+    print('I am thinking of a number between 1 and 10.');
+elif playAgain == 'n':
+    print('Thanks for playing!');
+
+# check if the user input a invalid response
+else:
+    print('Invalid response!');
